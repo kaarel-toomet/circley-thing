9e3e7c7d6c9fa7ca55589bb0603f4e58b6f3ed42
kaarel <kaarel.toomet@gmail.com>
2018-04-22 09:49:24 -0700

    collision detection

--- circlevolution.py
+++ circlevolution.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import random as r
+import sys
 pg.init()
 pg.mixer.init()
 pic = pg.image.load("trollface.png")
@@ -27,7 +28,6 @@ pfont = pg.font.SysFont("Times", 50)
 pause = False
 gameover = False
 player = pg.sprite.Group()
-discs = pg.sprite.Group()
 class Player(pg.sprite.Sprite):
     def __init__(self,x,y):
         pg.sprite.Sprite.__init__(self)
@@ -60,9 +60,10 @@ class Player(pg.sprite.Sprite):
             self.rect.x -= dist
         if mright and right:
             self.rect.x += dist
-class Disc(pg.sprite.Sprite):
+class Disc(pg.sprite.GroupSingle):
     def __init__(self, x, y, xvel, yvel,r,g,b,wr,wg,wb,pickiness=pickiness):
-        pg.sprite.Sprite.__init__(self)
+        d = pg.sprite.Sprite()
+        pg.sprite.GroupSingle.__init__(self, d)
         self.x = x
         self.y = y
         self.xvel = xvel
@@ -74,6 +75,7 @@ class Disc(pg.sprite.Sprite):
         self.wg = wg
         self.wb = wb
         self.pickiness = pickiness
+        self.sprite.rect = pg.rect.Rect(x,y,20,20)
     def update(self):
         if self.x + self.xvel <= screenw-90 and self.x + self.xvel >= 0:
             self.x += self.xvel
@@ -85,6 +87,7 @@ class Disc(pg.sprite.Sprite):
             self.y = int(self.y)
         else:
             self.yvel = -self.yvel
+        self.sprite.rect = pg.rect.Rect(self.x,self.y,20,20)
     def drw(self):
         pg.draw.circle(screen,(self.r,self.g,self.b),(self.x,self.y),20)
 def reset():
@@ -95,12 +98,11 @@ def reset():
 #hullmyts = Player(screenw/2,screenh/2)
 #player.add(hullmyts)
 
-djsks = [ Disc(r.randint(100,screenw-190),r.randint(100,screenh-190),
+discs = [ Disc(r.randint(100,screenw-190),r.randint(100,screenh-190),
                r.randint(-10,10),r.randint(-10,10),
                r.randint(0,255),r.randint(0,255),r.randint(0,255),
                r.randint(0,255),r.randint(0,255),r.randint(0,255))
-            for i in range(21)]
-discs.add(djsks)
+            for i in range(5)]
 
 while do:
     for event in pg.event.get():
@@ -145,7 +147,6 @@ while do:
         screen.blit(text,text_rect)
         pg.display.update()
     if lifes == 0:
-        blap.play()
         uded = "GAME OVER"
         dtext = dfont.render(uded, True, (255,0,0))
         dtext_rect = dtext.get_rect()
@@ -164,6 +165,17 @@ while do:
                 if event.key == pg.K_r:
                     gameover = False
                     reset()
+    for i1, disc1 in enumerate(discs):
+        for i2, disc2 in enumerate(discs):
+            if disc1 is disc2:
+                #print("slef", i1, i2)
+                continue
+            col = pg.sprite.spritecollide(disc1.sprite,disc2,False)
+            #print(disc1,disc2, i1, i2)
+            #print(col)
+            if len(col) > 0:
+                print(disc1.x, disc2.x)
+                #sys.exit(0)
     screen.fill((128,128,128))
     score = ("Lifes: " + str(lifes))
     text = font.render(score, True, (255,255,255))
@@ -171,8 +183,8 @@ while do:
     text_rect.centerx = screen.get_rect().centerx
     text_rect.y = 10
     screen.blit(text,text_rect)
-    discs.update()
     for disc in discs:
+        disc.update()
         disc.drw()
     player.update(mup, mdown, mleft, mright)
     player.draw(screen)
